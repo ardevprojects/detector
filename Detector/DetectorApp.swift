71ee87da5ef1d0e49e93e98a49a9cbad8775dda7
ardevprojects <ardevprojects@gmail.com>
2024-04-03 23:12:26 +0200

    Initial Commit

---
+++ Detector/DetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DetectorApp.swift
+//  Detector
+//
+//  Created by A A on 03/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct DetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
